4902b5f5043f6e5447debd536cdca89548201193
Anthony Cho <antcho92@gmail.com>
2016-10-08 20:28:20 -0700

    working login

---
+++ README.md
@@ -0,0 +1,7 @@
+Login and registration
+
+Functionality:
+1. Registration
+
+Things that need work:
+1. Need to create password validation
--- server.py
+++ server.py
@@ -4,7 +4,8 @@ from mysqlconnection import MySQLConnector
 #regex
 import re
 email_regex = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-name_regex = re.compile(r'')
+#found a name regex from online
+name_regex = re.compile(r'^[A-Z][-a-zA-Z]+$')
 #import bcrypt
 from flask.ext.bcrypt import Bcrypt
 
@@ -18,15 +19,42 @@ mysql = MySQLConnector(app, 'login_and_registration')
 @app.route('/')
 def index():
     return render_template('/index.html')
+@app.route('/login', methods=['POST'])
+def login():
+    user_query = "SELECT * FROM users WHERE email = :email"
+    user_data = {
+        'email': request.form['email']
+    }
+    user = mysql.query_db(user_query, user_data)[0]
+    print(user['pw_hash'])
+    if bcrypt.check_password_hash(user['pw_hash'], request.form['pw']):
+        flash("Password Correct")
+        return render_template('/success.html')
+    else:
+        flash("Wrong password")
+        return redirect('/')
 
+# Handles Registration
 @app.route('/register', methods=['POST'])
 def registration():
     print(request.form)
-    if len(request.form['first_name']) < 2 or len(request.form['last_name']) < 2:
-        flash("Name must be 2 chars or longer")
-    elif not email_regex.match(request.form['email']):
+    if not name_regex.match(request.form['first_name']) or not name_regex.match(request.form['last_name']):
+        flash("Invalid first or last name")
+    if not email_regex.match(request.form['email']):
         flash("Invalid Email Address!")
-    elif
+    else:
+        flash("New user successfully registered")
+        pw_hash = bcrypt.generate_password_hash(request.form['pw'])
+        print(pw_hash)
+        query = "INSERT INTO users (first_name, last_name, email, pw_hash, created_at, updated_at) VALUES (:first_name, :last_name, :email, :pw_hash, Now(), Now());"
+        data = {
+            'first_name': request.form['first_name'],
+            'last_name': request.form['last_name'],
+            'email': request.form['email'],
+            'pw_hash': pw_hash
+        }
+        mysql.query_db(query, data)
+        return render_template('/success.html')
     return redirect('/')
 
 app.run(debug=True)
